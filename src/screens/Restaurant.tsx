af1a5f5151bbf08bfb96a27e12fe24ef4af6d1b3
Guilherme Chaves Volpato <volpato2103@gmail.com>
2024-04-05 15:28:17 -0300

    feat: adicionado stacks para as rotas bottom tabs, concluindo assim a configuração inicial de rotas

---
+++ src/screens/Restaurant.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export function Restaurant() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Restaurante Tela</Text>
+    </View>
+  );
+}
