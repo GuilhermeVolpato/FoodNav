f89537cd8250f0a75ec01e8fcb328d8ff502b316
Guilherme Chaves Volpato <guilherme.94720@alunosatc.edu.br>
2024-06-10 21:27:28 -0300

    novo slide

--- slides.js
+++ slides.js
@@ -3,20 +3,20 @@ export default [
     key: "1",
     title: "Bem vindo!",
     description: "Aqui você encontrará os melhores restaurantes da cidade que é a sua cara!",
-    image: require("@assets/arlivre.jpg"),
+    image: require("@assets/1.jpg"),
   },
   {
     key: "2",
     title: "Simples e fácil de usar",
     description:
       "Esse aplicativo foi pensado para ser simples e fácil de usar, para que você possa encontrar o que precisa rapidamente!",
-    image: require("@assets/bar.jpg"),
+    image: require("@assets/2.jpg"),
   },
   {
     key: "3",
     title: "Localização precisa para facilitar a sua vida!",
     description: "Utilizamos a localização do seu dispositivo para encontrar os melhores restaurantes perto de você!",
-    image: require("@assets/cafe.jpg"),
+    image: require("@assets/3.jpg"),
   },
   {
     key: "4",
